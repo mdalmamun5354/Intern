5b50c64177d3ed1c9f6afb2c5fdc599da0bb26f2
Md. Al Mamun <mamun.dev.pro@gmail.com>
2024-07-10 15:30:42 +0600

    fashionbazaar frontend added

--- class projects/fashionbazaar/resources/views/backend/addProduct.blade.php
+++ class projects/fashionbazaar/resources/views/backend/addProduct.blade.php
@@ -5,7 +5,7 @@
     <div class="page-header min-vh-75">
         <div class="container">
             <div class="row">
-                <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+                <div class="col-xl-6 col-lg-5 col-md-6 d-flex flex-column mx-auto">
                     <div class="card card-plain">
                         <div class="card-header pb-0 text-left bg-transparent">
                             <h3 class="font-weight-bolder text-info text-gradient">Add Product</h3>
---
+++ class projects/fashionbazaar/resources/views/frontend/home.blade.php
@@ -0,0 +1,9 @@
+@extends('frontend.layouts.master')
+@section('content')
+    @include('frontend.layouts.sections.hero')
+    @include('frontend.layouts.sections.why')
+    @include('frontend.layouts.sections.arrival')
+    @include('frontend.layouts.sections.products')
+    @include('frontend.layouts.sections.subscribe')
+    @include('frontend.layouts.sections.client')
+@endsection
