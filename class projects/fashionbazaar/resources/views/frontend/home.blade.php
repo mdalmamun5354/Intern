4cff7026df497eec38c559de455d96384ae56ee1
Md. Al Mamun <mamun.dev.pro@gmail.com>
2024-07-14 16:04:49 +0600

    class project moved

--- class projects/fashionbazaar/resources/views/frontend/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('frontend.layouts.master')
-@section('content')
-    @include('frontend.layouts.sections.heroSection')
-    @include('frontend.layouts.sections.whySection')
-    @include('frontend.layouts.sections.arrivalSection')
-    @include('frontend.layouts.sections.productsSection')
-    @include('frontend.layouts.sections.subscribeSection')
-    @include('frontend.layouts.sections.clientSection')
-@endsection
